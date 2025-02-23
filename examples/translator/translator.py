f0bebb828a0e1c5be0ce37dd76612ebdaf257309
Amayuru Amarasinghe <116435706+Amayuru1999@users.noreply.github.com>
2025-02-23 23:28:01 +0530

    init: Project initialize setup

--- examples/translator/translator.py
+++ examples/translator/translator.py
@@ -1,26 +1,28 @@
-import asyncio
+# translator.py
 
-from coagent.agents import ChatAgent
+import asyncio
+import os
+
+from coagent.agents import ChatAgent, ModelClient
 from coagent.core import AgentSpec, idle_loop, new, set_stderr_logger
 from coagent.runtimes import NATSRuntime
-
 
 translator = AgentSpec(
     "translator",
     new(
         ChatAgent,
-        system="""You are a professional translator that can translate Chinese to English.""",
+        system="You are a professional translator that can translate Chinese to English.",
+        client=ModelClient(model="openai/gpt-4o", api_key=os.getenv("OPENAI_API_KEY")),
     ),
 )
 
 
 async def main():
-    async with NATSRuntime.from_servers() as runtime:
+    async with NATSRuntime.from_servers("nats://localhost:4222") as runtime:
         await runtime.register(translator)
         await idle_loop()
 
 
 if __name__ == "__main__":
-    set_stderr_logger("TRACE")
-
+    set_stderr_logger()
     asyncio.run(main())
